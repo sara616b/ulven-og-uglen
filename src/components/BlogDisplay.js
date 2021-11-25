6e6f37f9b92f84789fff3dbff7860fa80ab47b3d
Victoria <vigi2586@stud.kea.dk>
2021-11-25 13:00:51 +0100

    styled blog side

--- src/components/BlogDisplay.js
+++ src/components/BlogDisplay.js
@@ -1,8 +1,17 @@
 export default function BlogDisplay({ blog }) {
   return (
     <div className="blog-element">
-      <img className="blogBillede" src={blog.topbillede.guid} alt="image" />
-      <h3>{blog.title.rendered}</h3>
+      <div className="blog-background">
+        <div className="blog-content">
+            <img 
+            className="blogBillede" 
+            src={blog.topbillede.guid} 
+            alt="image" />
+            <div className="blog-info">
+              <h3>{blog.title.rendered}</h3>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,11 +3,11 @@ import BlogDisplay from "../components/BlogDisplay";
 export default function Blog({ props }) {
   return (
     <div>
-      <h2>Blog</h2>
+      <h2 className="blog-titel">Blog</h2>
       {props.blogindlg !== undefined ? (
         <div>
           {props.blogindlg.length > 0 ? (
-            <div>
+            <div className="Blogs">
               {props.blogindlg.map((blog) => {
                 return <BlogDisplay key={blog.id} blog={blog} />;
               })}
