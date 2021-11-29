440956fadac4e755026d7931e6349e1b2a03182f
Victoria <vigi2586@stud.kea.dk>
2021-11-29 13:48:56 +0100

    startede på om forlaget

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,23 @@
 export default function About() {
-  return <div>Om Forlaget</div>;
+  return (
+
+    <div className="aboutS">
+      <div>
+        <h1>Om Forlaget</h1>
+
+        <p>
+          Ideen om Ulven og Uglen har været længe undervejs, og i sommeren 2013 blev forlaget en realitet. Ulven og Uglens ambition er at udgive fantasy af høj kvalitet til et voksent publikum, og forlaget vil fokusere på både originale danske værker og på danske oversættelser af udenlandske værker. 
+        </p>
+
+        <p> 
+          Det er vores indtryk at den voksne læser i Danmark mangler et bredt udvalg af fantasylitteratur i en høj kvalitet, og det er vores mål at bidrage med det. 
+        </p>
+          
+        <p> 
+          Fantasy for voksne er på mange måder en forsømt genre på det danske bogmarked, ligesom fantasygenren af mange opfattes som en på mange måder underlødig eller populistisk genre der mest er for børn. Denne holdning vil vi gerne være med til at ændre for vi mener at fantasygenren har mange kvaliteter som bliver overset i daglig brug. Læs om fantasygenren.
+        </p>
+      </div>
+    </div>
+
+  );
 }
